86b44d9ce5897bf6836da67b6558216adbda2bbf
tutorial <videotutoriales@hotmail.com>
2012-09-18 17:40:24 -0400

    master card 1987, visa 4658,

---
+++ cajero/card.java
@@ -0,0 +1,9 @@
+
+package cajero.macg;
+
+
+public class card {
+    String card[]={"master card", " maestro","visa", "american express"};
+    int saldopordia[]= {2000,2000,2000,20000};
+    
+}
---
+++ cajero/papeletas.java
@@ -0,0 +1,12 @@
+
+package cajero.macg;
+
+
+public class papeletas {
+    int cien=150;
+    int doscientos=100;
+    int quinientos=50;
+    int mil=20;
+    int dosmil=10;
+           
+}
